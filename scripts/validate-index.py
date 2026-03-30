c0c9dd5f23e49c6003aaaf13cdb35767e6b7d1c7
axeprpr <axe3@qq.com>
2026-03-30 12:10:26 +0800

    fix: update validate-index.py to use _site/index.yaml

--- scripts/validate-index.py
+++ scripts/validate-index.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 import yaml, sys
-with open('packages/index.yaml') as f:
+with open('_site/index.yaml') as f:
     idx = yaml.safe_load(f)
 for name, versions in idx.get('entries', {}).items():
     print(name + ': ' + str([v['version'] for v in versions]))
